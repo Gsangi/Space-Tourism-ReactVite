810ee16b11238af8ed4c1c312689fbca3fd18a43
Gsangi <sanskar6053@gmail.com>
2023-09-18 07:01:45 +0530

    Initial Setup

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css"
+
+const Home = () => {
+  return (
+    <section class="home-section">
+      <div class="home-content">
+        <h3>SO, YOU WANT TO TRAVEL TO</h3>
+        <h1>SPACE</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <button class="home-content-button">EXPLORE</button>
+    </section>
+  );
+};
+
+export default Home
